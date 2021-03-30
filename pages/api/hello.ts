768d9f9158cb1d7d005b227bc558d4c8879a41b7
Alan R. Soares <alanrsoars@gmail.com>
2021-03-31 08:56:38 +1300

    set up typescript

---
+++ pages/api/hello.ts
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function helloAPI(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({
+    name: req.query.name ?? "John Doe",
+  });
+}
